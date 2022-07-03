ca3592ef844a6128872ae85c1c3d781cb4f7bdf5
abelo15 <abiyuh17@gmail.com>
2022-07-03 04:00:44 -0700

    Hotfix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -12,6 +11,7 @@ int main(void)
 	a = 'a';
 
 	while(a <= 'z')
+
 	{
 		putchar(a);
 		putchar('\n');
