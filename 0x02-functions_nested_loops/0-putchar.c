87f34beb631edbb28b82924d9bfcdb75eaeb50cf
abelo15 <abiyuh17@gmail.com>
2022-07-05 08:25:34 -0700

    questions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry function
  * Write a program that prints _putchar, followed by a new line.
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * main - prints putchar
+ * return: always 0
+ */
+
+int main(void)
+{
+	_putcha("_");
+	_putcha("p");
+        _putcha("u");
+        _putcha("t");                                                                                       _putcha("c");                                                                                       _putcha("h");
+	_putcha("a");
+        _putcha("r");                                                                                       _putcha("\n");                                                                                	return (0);
+}
+
+
+
+
+
