6bb90f019c83d18a3e177c832ef08bc4253b61f2
abelo15 <abiyuh17@gmail.com>
2022-07-07 05:01:54 -0700

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,19 +5,17 @@
  * where n is the number of theimes the character _ should be printed
  * the line should end with a /n
  * if n is 0 or less, the function should only print \n
- */
+ * Return: Always 0
+*/
 
 void print_line(int n)
 {
-	int x = 0;
+	int i = 0;
 
-	if(n > 0)
+	if (n > 0)
 	{
-		for(;x < n; x++)
-		{
+		for (; i < n; i++)
 			_putchar('_');
-		}
 	}
-	_putchar('$');
 	_putchar('\n');
 }
