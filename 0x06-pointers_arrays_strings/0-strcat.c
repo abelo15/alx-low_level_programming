67ed8252938841b109ba6837823a7ea632d9fbfd
abelo15 <abiyuh17@gmail.com>
2022-07-14 02:19:48 -0700

    added readme and first question

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	char *temp = dest;
 
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
-	{
-		i++;
-		j++;
-	}
-	return (dest);
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
 }
